1b91cc7588ae35f6eacb70dbba221dbba89217a9
Tyler Draughon <tdraughon@byu.edu>
2017-02-02 20:13:57 -0700

    login handler
    Login handler implemented

--- server/src/main/java/com/floorcorn/tickettoride/handlers/LoginHandler.java
+++ server/src/main/java/com/floorcorn/tickettoride/handlers/LoginHandler.java
@@ -1,8 +1,13 @@
 package com.floorcorn.tickettoride.handlers;
 
+import com.floorcorn.tickettoride.Serializer;
+import com.floorcorn.tickettoride.ServerFacade;
+import com.floorcorn.tickettoride.communication.Results;
+import com.floorcorn.tickettoride.model.User;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 /**
  * Created by Tyler on 1/31/2017.
@@ -12,6 +17,22 @@ public class LoginHandler extends HandlerBase {
 
 	@Override
 	public void handle(HttpExchange httpExchange) throws IOException {
+		try {
+			String reqBody = getRequestBody(httpExchange);
+			if(reqBody == null || reqBody.isEmpty()) {
+				httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, -1);
+				return;
+			}
+			User userInfo = Serializer.getInstance().deserializeUser(reqBody);
 
+			userInfo = ServerFacade.getInstance().login(userInfo);
+
+			httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+			sendResponseBody(httpExchange, new Results(true, userInfo));
+
+		} catch(IOException e){
+			e.printStackTrace();
+			httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, -1);
+		}
 	}
 }
