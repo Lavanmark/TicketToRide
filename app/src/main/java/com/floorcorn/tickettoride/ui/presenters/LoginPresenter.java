0efbb038d5d601e4c1576ba1079e86324765f348
mgardanier <mgardanier@gmail.com>
2017-02-04 14:19:53 -0700

    Cleaned up presenter/view duplicates and added javadoc/dbc comments. Created LoginView and LoginPresenter classes

---
+++ app/src/main/java/com/floorcorn/tickettoride/ui/presenters/LoginPresenter.java
@@ -0,0 +1,29 @@
+package com.floorcorn.tickettoride.ui.presenters;
+
+import com.floorcorn.tickettoride.ui.views.ILoginView;
+import com.floorcorn.tickettoride.ui.views.IView;
+
+/**
+ * Created by mgard on 2/4/2017.
+ */
+
+public class LoginPresenter implements IPresenter {
+
+    private ILoginView loginView;
+
+    /**
+     * Default constructor for LoginPresenter class
+     *
+     * @post a new instance of LoginPresenter is created.
+     */
+    public LoginPresenter() {}
+
+    @Override
+    public void setView(IView view) {
+        if(view instanceof ILoginView) {
+            this.loginView = (ILoginView)view;
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+}
