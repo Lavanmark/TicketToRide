003eaba77ab9d5484bc920b7ae44e93bdf2de6ea
lily514 <l.hamelaswood@gmail.com>
2017-02-08 13:58:15 -0700

    few updates. Prep for merge

--- app/src/main/java/com/floorcorn/tickettoride/CreateGameActivity.java
+++ app/src/main/java/com/floorcorn/tickettoride/CreateGameActivity.java
@@ -3,11 +3,23 @@ package com.floorcorn.tickettoride;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateGameActivity extends AppCompatActivity {
+
+    List<String> colorSpinnerArray;
+    List<String> numPlayerArray;
+    IView LobbyView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
+        colorSpinnerArray = LobbyView
+        numPlayerArray;
     }
+
+
 }
