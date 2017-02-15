082372b1d110c4774645daf1a165de172c384403
Tyler Draughon <tdraughon@byu.edu>
2017-02-15 01:47:48 -0700

    populating player list
    populating player list

--- app/src/main/java/com/floorcorn/tickettoride/ui/views/activities/PregameActivity.java
+++ app/src/main/java/com/floorcorn/tickettoride/ui/views/activities/PregameActivity.java
@@ -7,8 +7,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.floorcorn.tickettoride.R;
@@ -18,6 +21,7 @@ import com.floorcorn.tickettoride.ui.presenters.PregamePresenter;
 import com.floorcorn.tickettoride.ui.views.IPregameView;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -27,7 +31,7 @@ import java.util.Set;
 public class PregameActivity extends AppCompatActivity implements IPregameView {
 
     private PregamePresenter presenter;
-    private RecyclerView playerList;
+    private RecyclerView playerListView;
     private Button cancelGameButton;
 
     @Override
@@ -39,7 +43,7 @@ public class PregameActivity extends AppCompatActivity implements IPregameView {
 
         presenter = new PregamePresenter();
 
-        playerList = (RecyclerView) findViewById(R.id.pregame_player_list);
+	    playerListView = (RecyclerView) findViewById(R.id.pregame_player_list);
         cancelGameButton = (Button) findViewById(R.id.cancelGameButton);
 
         if(presenter.isConductor())
@@ -71,21 +75,15 @@ public class PregameActivity extends AppCompatActivity implements IPregameView {
     }
 
     /**
-     * NOTE: I don't think this function is necessary. The Pregame view *is* the game waiting
-     * dialog.
-     */
-    @Override
-    public void createGameWaitingDialog() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
      * Displays this game's players in the view.
      * @param players A Set of Player objects representing players in current game
      */
     @Override
     public void displayPlayerList(ArrayList<Player> players) {
-
+	    playerListView = (RecyclerView) findViewById(R.id.pregame_player_list);
+	    assert playerListView != null;
+	    PlayerListRecyclerViewAdapter a = (PlayerListRecyclerViewAdapter) ((RecyclerView) playerListView).getAdapter();
+	    a.swapList(players);
     }
 
     /**
@@ -124,4 +122,62 @@ public class PregameActivity extends AppCompatActivity implements IPregameView {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
+	public class PlayerListRecyclerViewAdapter extends RecyclerView.Adapter<PlayerListRecyclerViewAdapter.ViewHolder> {
+		private RecyclerView rv;
+		private List<Player> players;
+		private int mSelectedPosition;
+		private View mSelectedView;
+
+		PlayerListRecyclerViewAdapter(RecyclerView rv, List<Player> items) {
+			mSelectedPosition = -1;
+			this.rv = rv;
+		}
+
+		@Override
+		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View view = LayoutInflater.from(parent.getContext())
+					.inflate(R.layout.game_list_content, parent, false);
+			return new ViewHolder(view);
+		}
+
+		void swapList(List<Player> list) {
+			players.clear();
+			players.addAll(list);
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public void onBindViewHolder(final ViewHolder holder, int position) {
+			holder.mItem = (Player) players.get(position);
+			if(holder.mItem == null)
+				return;
+			holder.mNameView.setText(holder.mItem.getName());
+			holder.mColorView.setText(String.valueOf(holder.mItem.getColor()));
+		}
+
+		@Override
+		public int getItemCount() {
+			return players.size();
+		}
+
+		class ViewHolder extends RecyclerView.ViewHolder {
+			final View mView;
+			final TextView mNameView;
+			final TextView mColorView;
+			Player mItem;
+
+			ViewHolder(View view) {
+				super(view);
+				mView = view;
+				mNameView = (TextView) view.findViewById(R.id.playerListNameText);
+				mColorView = (TextView) view.findViewById(R.id.playerListColorText);
+				view.setClickable(false);
+			}
+
+			@Override
+			public String toString() {
+				return super.toString() + " '" + mNameView.getText() + "'";
+			}
+		}
+	}
 }
