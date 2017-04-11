d2f2cb0d7c9e0868279a2b61221b86d1c29f3edf
lily514 <l.hamelaswood@gmail.com>
2017-04-10 19:05:02 -0600

    dto fixes

--- persistence/src/main/java/com/floorcorn/tickettoride/ICommandDAO.java
+++ persistence/src/main/java/com/floorcorn/tickettoride/ICommandDAO.java
@@ -1,4 +1,11 @@
 package com.floorcorn.tickettoride;
 
+import java.util.List;
+
 public interface ICommandDAO extends IDAO{
+
+    boolean create(ICommandDTO dto);
+    boolean update(ICommandDTO dto);
+    List<ICommandDTO> getAll();
+    boolean delete(ICommandDTO dto);
 }
--- persistence/src/main/java/com/floorcorn/tickettoride/IUserDAO.java
+++ persistence/src/main/java/com/floorcorn/tickettoride/IUserDAO.java
@@ -1,4 +1,10 @@
 package com.floorcorn.tickettoride;
 
+import java.util.List;
+
 public interface IUserDAO extends IDAO {
+    boolean create(IUserDTO dto);
+    boolean update(IUserDTO dto);
+    List<IUserDTO> getAll();
+    boolean delete(IUserDTO dto);
 }
